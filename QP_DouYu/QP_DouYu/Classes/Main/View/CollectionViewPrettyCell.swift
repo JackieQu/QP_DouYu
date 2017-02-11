d18fb4cfd8d844994dea0953ae5977f69f53e9a8
JackieQu <13263699826@163.com>
2017-02-11 23:58:56 +0800

    Creat CollectionViewPrettyCell.xib

---
+++ QP_DouYu/QP_DouYu/Classes/Main/View/CollectionViewPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewPrettyCell.swift
+//  QP_DouYu
+//
+//  Created by 曲鹏 on 2017/2/11.
+//  Copyright © 2017年 JackieQu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
