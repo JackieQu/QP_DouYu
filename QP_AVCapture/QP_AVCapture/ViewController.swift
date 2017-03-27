ac3b46b8d6d9faf593c0c0554522a848725850d8
JackieQu <13263699826@163.com>
2017-03-27 16:33:49 +0800

    Sort out AVCapture

--- QP_AVCapture/QP_AVCapture/ViewController.swift
+++ QP_AVCapture/QP_AVCapture/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
     fileprivate lazy var session : AVCaptureSession = AVCaptureSession()
     fileprivate lazy var previewLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.session)
     
-//    fileprivate var connection : AVCaptureConnection?
     fileprivate var videoOutput : AVCaptureVideoDataOutput?
     fileprivate var videoInput : AVCaptureDeviceInput?
     fileprivate var movieOutput : AVCaptureMovieFileOutput?
@@ -42,11 +41,7 @@ extension ViewController {
         connection?.preferredVideoStabilizationMode = .auto
 
         // 4.设置预览图层
-//        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
-//        previewLayer?.frame = view.bounds
-//        view.layer.addSublayer(previewLayer!)
         previewLayer.frame = view.bounds
-//        view.layer.addSublayer(previewLayer)
         view.layer.insertSublayer(previewLayer, at: 0)
         
         // 5.开始采集
@@ -87,57 +82,34 @@ extension ViewController {
         session.addInput(videoInput)
         session.commitConfiguration()
         
-        print("切换镜头")
         self.videoInput = videoInput
+        print("切换镜头")
     }
 }
 
 extension ViewController {
     fileprivate func setupVideo() {
-        // 1.创建捕捉会话
-        //        let session = AVCaptureSession()
-        //        self.session = session
-        
-        // 2.设置输入源(摄像头)
-        // 2.1.获取摄像头设备
+        // 1.设置输入源(摄像头)
+        // 1.1.获取摄像头设备
         guard let devices = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo) as? [AVCaptureDevice] else {
-            
             print("摄像头不可用")
             return
         }
-        
-        /*
-         var device : AVCaptureDevice!
-         for d in devices {
-         if d.position == .front {
-         device = d
-         break
-         }
-         }
-         */
-        /*
-         let device = devices.filter { (device : AVCaptureDevice) -> Bool in
-         return device.position == .front
-         }.first
-         */
-        
         guard let device = devices.filter({ $0.position == .front }).first else { return }
         
-        // 2.2.通过 device 创建 AVCaptureInput 对象
+        // 1.2.通过 device 创建 AVCaptureInput 对象
         guard let videoInput = try? AVCaptureDeviceInput(device: device) else { return }
         self.videoInput = videoInput
         
-        // 2.3.将 input 添加到会话中
+        // 1.3.将 input 添加到会话中
         session.addInput(videoInput)
         
-        // 3.设置输出源
+        // 2.设置输出源
         let videoOutput = AVCaptureVideoDataOutput()
         videoOutput.setSampleBufferDelegate(self, queue: videoQueue)
-        
         session.addOutput(videoOutput)
         
-        // 4.获取 video 对应的 connection
-//        connection = videoOutput.connection(withMediaType: AVMediaTypeVideo)
+        // 3.获取 video 对应的 connection
         self.videoOutput = videoOutput
     }
     
@@ -145,10 +117,8 @@ extension ViewController {
         // 1.设置输入源(麦克风)
         // 1.1.获取设备麦克风
         guard let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio) else { return }
-        
         // 1.2.根据 device 创建 AVCaptureInput
         guard let audioInput = try? AVCaptureDeviceInput(device: device) else { return }
-        
         // 1.3.将 input 添加到会话中
         session.addInput(audioInput)
         
@@ -170,7 +140,7 @@ extension ViewController : AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptu
     }
 }
 
-// MARK:- 
+// MARK:- 写入文件
 extension ViewController : AVCaptureFileOutputRecordingDelegate {
     func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
         print("开始写入文件")
